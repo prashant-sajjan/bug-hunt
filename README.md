7595054096d2771a71576c7b9a58a2fd861c0adc
PrashantSajjan1984 <prashant.sajjan@gmail.com>
2019-01-26 11:45:34 -0600

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# bug-hunt
-Bug Hunt Framework - A Keyword Driven Java Framework (WIP)
+# Bug Hunt Automation Framework (Keyword Driven Framework)
+
+Bug Hunt Framework is a Java based keyword driven framework which supports any Java based automation tool.
+It supports executing test cases in parallel, running test cases on multiple browsers/ devices.
+
+## Installation
+From your repositories folder on your local machine:
+
+  - $ `git clone xxx`
+  - import buh-hunt project as maven project in any JAVA IDE of your choice
+
+## Running the tests
+- Select tests to be executed in TestManager.xls
+- Running through IDE - Open BugHuntRunner.java and run as Java application
+- Command Line - mvn clean compile exec:java -Dexec.mainClass="com.bughunt.runner.BugHuntRunner"
+
+## Automating Tests
+- Follow steps in User_Guide.doc
+
+
+
--- src/main/java/com/bughunt/runner/BugHuntRunner.java
+++ src/main/java/com/bughunt/runner/BugHuntRunner.java
@@ -16,33 +16,7 @@ public class BugHuntRunner {
 	public static void main(String [] args) throws InterruptedException, ParseException {
 		ExecutionManager.instance().triggerExecution();
 		System.out.println("Automation execution completed");
-		// createSearchKeywords();
-		// createHomePageKeywords();
+		System.exit(0); // For Maven command line execution fix
 	}
 
-	private static void createHomePageKeywords() {
-		for(int i=2; i<=20; i++) {
-			String s =	String.format("public void openHomePage%d() { \n" +
-					"    homePage.openHomePage();\n" + 
-					"    report.addReportStep(\"PASS STEP\", \"Home Page %d is displayed\", StepResult.PASS);\n" +
-					"    if(failTest) {\n" + 
-					"	    report.addReportStep(\"FAIL STEP\", \"Home Page 2 is displayed\", StepResult.FAIL);\n" + 
-					"    }\n" +
-					"}", i, i);
-		
-			System.out.println(s);
-			System.out.println();
-		}
-	}
-	
-	private static void createSearchKeywords() {
-		for(int i=2; i<=20; i++) {
-			String s =	String.format("public void searchKeyword%d() { \n"+
-					"    report.addReportStep(\"PASS STEP\", \"Search Keyword %d\", StepResult.PASS);\n"
-					+ "}", i, i);
-		
-			System.out.println(s);
-			System.out.println();
-		}
-	}
 }
